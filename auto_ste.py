cb4b3d0e8b84426010bfc33a4fc2cd66536cf45e
robinentjes <65961978+robinentjes@users.noreply.github.com>
2021-05-02 17:42:57 +0200

    added the files for both autoencoders

---
+++ auto_ste.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from typing import List, Tuple
+import requests
+from zipfile import ZipFile
+INPUTVECTORSIZE = 300
+BINARYVECTORSIZE = 512
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+class STEFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        return (input > 0).float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return F.hardtanh(grad_output)
+
+class StraightThroughEstimator(nn.Module):
+    def __init__(self):
+        super(StraightThroughEstimator, self).__init__()
+
+    def forward(self, x):
+        x = STEFunction.apply(x)
+        return x
+
+# autoencoder class
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(INPUTVECTORSIZE, BINARYVECTORSIZE),
+            StraightThroughEstimator())
+        self.decoder = nn.Linear(BINARYVECTORSIZE, INPUTVECTORSIZE)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        decoded = torch.tanh(self.decoder(x))
+
+        return decoded
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return 400000
+
+    def __getitem__(self, index):
+        # Select sample
+        X = self.data[index]
+        y = self.data[index]
+
+        return X, y
+
+# initialize model
+model = AutoEncoder()
+
+# initialize optimizer
+optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum = 0.95)
+
+# loss function
+lambdareg = 1.0/75.0 # divided by 75 because of batchsize
+mse = nn.MSELoss()
+
+#load data
+f = open("glove.6b/glove.6b.300d.txt", "r", encoding="utf8")
+lines = f.readlines()
+words = []
+embeddings: List[np.array] = []
+for i, line in enumerate(lines):
+    array = line.split()
+    word = array[0]
+    words.append(word)
+    embedding = np.array(list(map(float, array[1:])))
+    embeddings.append(embedding)
+
+embeddings = torch.Tensor(embeddings)
+data = Dataset(embeddings)
+word2idx = {word: idx for idx, word in enumerate(words)}
+idx2word = {idx: word for word, idx in word2idx.items()}
+dataset = torch.utils.data.DataLoader(data, batch_size=75, shuffle=True)
+
+# training loop
+running_loss = 0
+running_mseloss = 0
+for epoch in range(25):
+    print(epoch)
+    for i, batch in enumerate(dataset):
+        X, y = batch
+        model.zero_grad()
+        result = model(X)
+        lrec = mse(result, y)
+        loss = lrec
+        #running_mseloss += lrec
+        running_loss += loss
+        loss.backward()
+        optimizer.step()
+
+        if i % 500 == 499:    # print loss after every 500 cycles
+
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 500))
+                running_loss = 0.0
+
+binary = (model.encoder(embeddings).detach().numpy()).astype(np.uint
+
+# print to output file
+outputfilename = "ste" + str(BINARYVECTORSIZE) + ".txt"
+outputfile = open(outputfilename, "w", encoding="utf8")
+
+for word in words:
+    outputfile.write(word)
+    for dig in binary[word2idx[word]]:
+        outputfile.write(' ' + str(dig))
+
+    outputfile.write('\n')
+outputfile.close()
